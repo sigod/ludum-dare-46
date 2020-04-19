68825e7f907b90862692a5a02af267f35b1d8034
sigod <sigod.mail@gmail.com>
2020-04-19 13:16:23 +0300

    chore: set background to deep pink

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,8 @@ fn main() -> amethyst::Result<()> {
 			RenderingBundle::<DefaultBackend>::new()
 				.with_plugin(
 					RenderToWindow::from_config_path(display_config_path)?
-						.with_clear([0.34, 0.36, 0.52, 1.0]),
+						// NOTE: Deep pink.
+						.with_clear([1.0, 0.078, 0.576, 1.0]),
 				)
 				.with_plugin(RenderFlat2D::default()),
 		)?;
