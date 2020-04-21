117c14d38670902e8a42792718353491e7a2225f
morozv <psa.vitaly.moroz@gmail.com>
2020-04-21 03:31:28 +0300

    sounds fixed

--- src/constants.rs
+++ src/constants.rs
@@ -7,14 +7,14 @@ pub const AUTHOR: &str = "sigod & co";
 pub const DIMENSIONS: (f32, f32) = (1280.0, 800.0);
 pub const DESIRED_FPS: u32 = 70;
 
-pub const GUITAR_VOLUME: f32 = 0.30;
-pub const MAN_VOLUME: f32 = 0.30;
-pub const GIRL1_VOLUME: f32 = 0.30;
-pub const GIRL2_VOLUME: f32 = 0.30;
+pub const GUITAR_VOLUME: f32 = 0.55;
+pub const MAN_VOLUME: f32 = 0.28;
+pub const GIRL1_VOLUME: f32 = 0.36;
+pub const GIRL2_VOLUME: f32 = 0.36;
 pub const FIRE_VOLUME: f32 = 0.30;
 pub const OWL_VOLUME: f32 = 0.30;
-pub const STORY_VOLUME: f32 = 0.30;
-pub const FIREWOOD_THROW_VOLUME: f32 = 0.30;
+pub const STORY_VOLUME: f32 = 0.22;
+pub const FIREWOOD_THROW_VOLUME: f32 = 0.22;
 pub const BACKGROUND_MUSIC_VOLUME: f32 = 0.30;
 
 pub const STARTING_ANIMATION: AnimationId = AnimationId::BurnMedium;
